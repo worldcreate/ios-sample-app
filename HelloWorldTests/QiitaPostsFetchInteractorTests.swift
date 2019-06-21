bb49bb4a75c55c3a4022a71f00998b4a4f36609b
matsumoto <r-matsumoto@zyyx.jp>
2019-06-21 23:21:53 +0900

    unit-test 作成途中

---
+++ HelloWorldTests/QiitaPostsFetchInteractorTests.swift
@@ -0,0 +1,35 @@
+//
+//  QiitaPostsFetchInteractorTests.swift
+//  HelloWorldTests
+//
+//  Created by matsumoto on 2019/06/21.
+//  Copyright © 2019 zyyx. All rights reserved.
+//
+
+import XCTest
+
+class QiitaPostsFetchInteractorTests: XCTestCase {
+    
+    var interactor: QiitaPostsFetchInteractor? = nil
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
